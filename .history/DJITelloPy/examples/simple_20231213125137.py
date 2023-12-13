ce6dc41470011e4cb3fb7537c1802f52e94eb1bc
khulqu15 <khusnul.ninno@gmail.com>
2023-12-13 12:59:39 +0700

    dji

---
+++ .history/DJITelloPy/examples/simple_20231213125137.py
@@ -0,0 +1,13 @@
+from djitellopy import Tello
+
+tello = Tello()
+
+tello.connect()
+tello.takeoff()
+
+tello.move_left(50)
+tello.rotate_clockwise(90)
+tello.move_forward(50)
+tello.flip_forward()
+
+tello.land()
