6fc8023feb6667c6b1fea1fc0d53fc124411b138
Mohammad Khusnul Khuluq <khusnul.ninno15@gmail.com>
2024-02-19 23:26:25 +0900

    add program to check tello ip

---
+++ .history/trial/network-scanning_20240216223329.py
@@ -0,0 +1,16 @@
+import subprocess
+import ipaddress
+from subprocess import Popen, PIPE
+
+ip_net = ipaddress.ip_network(u'192.168.0.1/24', strict=False)
+
+for ip in ip_net.hosts():
+    ip = str(ip)
+    toping = Popen(['ping', '-c', '1', '-W', '50', ip], stdout=PIPE)
+    output = toping.communicate()[0]
+    hostalive = toping.returncode
+    
+    if hostalive ==0:
+        print(ip, "is online")
+    else:
+        print(ip, "is offline")
